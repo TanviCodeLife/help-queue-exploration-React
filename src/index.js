665508c872949869aa61564c931824e8fb0d50a2
Tanvi Garg <tanvi.garg23@gmail.com>
2019-02-10 23:14:54 -0800

    add basic react project structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var heading = React.createElement('h1', {}, 'Help Queue');
+var app = React.createElement('div', {}, heading);
+
+ReactDOM.render(
+  app,
+  document.getElementById('react-app-root')
+);
